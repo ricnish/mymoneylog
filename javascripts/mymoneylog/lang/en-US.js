b768606c15561f56c4296e6d570b346f9dd763a7
ricnishimura <ricnishimura@22c9decc-4b3f-0410-a08c-e77ef4b4d34b>
2008-02-01 16:39:08 +0000

    language correction
    git-svn-id: https://mymoneylog.googlecode.com/svn/trunk@13 22c9decc-4b3f-0410-a08c-e77ef4b4d34b

--- javascripts/mymoneylog/lang/en-US.js
+++ javascripts/mymoneylog/lang/en-US.js
@@ -4,6 +4,6 @@
 mlog.translation = {
 centschar             : '.',
 thousandchar          : ',',
-'datasample'          : '2000-01-01\t0.00\tinitial\t\tbank\n2000-01-01\t0.00\tinitial\t\tinvestiment\n2000-01-01\t0.00\tinitial\t\twallet\n2000-01-01\t0.00\tinitial\t\tcredit card\n2000-01-01\t0.00\tinitial\tautomobile\t\n2000-01-01\t0.00\tinitial\tbank\t\n2000-01-01\t0.00\tinitial\teducation\t\n2000-01-01\t0.00\tinitial\tincome\t\n2000-01-01\t0.00\tinitial\tentertainment\t\n2000-01-01\t0.00\tinitial\tgrocery\t\n2000-01-01\t0.00\tinitial\thome\t\n2000-01-01\t0.00\tinitial\tothers\t\n2000-01-01\t0.00\tinitial\thealthcare\t\n2000-01-01\t0.00\tinitial\tclothing\t\n2000-01-01\t0.00\tinitial\tgifts\t\n',
+'datasample'          : '2000-01-01\t0.00\tinitial\t\tbank\n2000-01-01\t0.00\tinitial\t\tinvestment\n2000-01-01\t0.00\tinitial\t\twallet\n2000-01-01\t0.00\tinitial\t\tcredit card\n2000-01-01\t0.00\tinitial\tautomobile\t\n2000-01-01\t0.00\tinitial\tbank\t\n2000-01-01\t0.00\tinitial\teducation\t\n2000-01-01\t0.00\tinitial\tincome\t\n2000-01-01\t0.00\tinitial\tentertainment\t\n2000-01-01\t0.00\tinitial\tgrocery\t\n2000-01-01\t0.00\tinitial\thome\t\n2000-01-01\t0.00\tinitial\tothers\t\n2000-01-01\t0.00\tinitial\thealth\t\n2000-01-01\t0.00\tinitial\tclothing\t\n2000-01-01\t0.00\tinitial\tgifts\t\n',
 'help text'           : '<h3>Sorry...</h3>English help still missing.<br/><br/><br/>'
 };
