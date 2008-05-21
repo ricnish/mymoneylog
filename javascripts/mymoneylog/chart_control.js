4b2dc7cad129d2fddd1f797ac45369fd24925fe9
ricnishimura <ricnishimura@22c9decc-4b3f-0410-a08c-e77ef4b4d34b>
2008-05-21 03:27:32 +0000

    overview chart added, to display only debit, credit, balance and accumulated values.
    git-svn-id: https://mymoneylog.googlecode.com/svn/trunk@72 22c9decc-4b3f-0410-a08c-e77ef4b4d34b

--- javascripts/mymoneylog/chart_control.js
+++ javascripts/mymoneylog/chart_control.js
@@ -11,46 +11,78 @@ mlog.chartControl = function() {
       var showDebits = eval($('#drawValuesKind').val());
       var xTicks = []; // x labels
       var i = 0;
-      var list = data.categories;
+      var list;
       var strDataset = '[';
       var count = 0;
+      var chartTitle = '';
       /* get selected categories */
       var categoriesChecked = [];
       $.each($('#show_ov_categories input:checked'), function() {
         categoriesChecked.push($(this).attr('title'));
       });
-      if (categoriesChecked.length==0) return; // return if none
 
-      for (var category in list) {
-        /* if not checked skip */
-        if ($.inArray(category,categoriesChecked)<0) continue;
-        if (i == 0) {
-          /* build x labels */
-          /* as: [[0, '2008-01'],[1, '2008-02']]... */
-          var str = '[';
+      /* build x labels */
+      /* as: [[0, '2008-01'],[1, '2008-02']]... */
+      var str = '[';
+      for (var month in data.summary[mlog.translator.get('balance')]) {
+        str += '['+count+', "'+month+'"],';
+        count++;
+      }
+      str = str.slice(0,str.length-1) + ']';
+      xTicks = eval(str);
+
+      // if any category selected: draw category chart
+      if (categoriesChecked.length>0) {
+        list = data.categories;
+        chartTitle = showDebits?mlog.translator.get('expenses by category'):mlog.translator.get('credits by category');
+        for (var category in list) {
+          /* if not checked skip */
+          if ($.inArray(category,categoriesChecked)<0) continue;
+          i++;
+          strDataset += '{label: "'+category+'", data: [';
+          count = 0;
+          str = '';
+          /* build category month's values */
+          /* eg: [[0,100],[1,95]], ... */
+          var tmpValue=0;
           for (var month in list[category]) {
-            str += '['+count+', "'+month+'"],';
+            tmpValue = Math.round(list[category][month]);
+            tmpValue = tmpValue * (showDebits?-1:1)
+            // just display debits
+            tmpValue = (tmpValue>0)?tmpValue:0;
+            str += '['+count+', '+tmpValue+'],';
             count++;
           }
-          str = str.slice(0,str.length-1) + ']';
-          xTicks = eval(str);
+          strDataset += str.slice(0,str.length-1) + ']},';
         }
-        i++;
-        strDataset += '{label: "'+category+'", data: [';
-        count = 0;
-        str = '';
-        /* build category month's values */
-        /* eg: [[0,100],[1,95]], ... */
-        var tmpValue=0;
-        for (var month in list[category]) {
-          tmpValue = Math.round(list[category][month]);
-          tmpValue = tmpValue * (showDebits?-1:1)
-          // just display debits
-          tmpValue = (tmpValue>0)?tmpValue:0;
-          str += '['+count+', '+tmpValue+'],';
-          count++;
+      } else {
+        // draw summary chart
+        list = data.summary;
+        chartTitle = mlog.translator.get('overview chart');
+        for (var description in list) {
+          i++;
+          strDataset += '{label: "'+description+'", data: [';
+          count = 0;
+          str = '';
+          /* build category month's values */
+          /* eg: [[0,100],[1,95]], ... */
+          var tmpValue=0;
+          // if debit show value as positive
+          if (description == mlog.translator.get('debit')) {
+            showDebits=true;
+          } else {
+            showDebits=false;
+          }
+          for (var month in list[description]) {
+            tmpValue = Math.round(list[description][month]);
+            tmpValue = tmpValue * (showDebits?-1:1)
+            // just display debits
+            //tmpValue = (tmpValue>0)?tmpValue:0;
+            str += '['+count+', '+tmpValue+'],';
+            count++;
+          }
+          strDataset += str.slice(0,str.length-1) + ']},';
         }
-        strDataset += str.slice(0,str.length-1) + ']},';
       }
       strDataset = strDataset.slice(0,strDataset.length-1) + ']';
       // Define a dataset.
@@ -59,7 +91,6 @@ mlog.chartControl = function() {
 
       // chart container
       var size = $('#chart').width()-25;
-      var chartTitle = showDebits?mlog.translator.get('expenses by category'):mlog.translator.get('credits by category');
       $('#chart').html('<h1>'+ chartTitle +
         '</h1><div id="chart_canvas" style="height:'+
         (size/1.75)+'px; width:'+(size)+'px;"></div>');
