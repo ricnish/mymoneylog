c02ccd9976619865dfee480e891ba4d4af7dba56
ricnishimura <ricnishimura@22c9decc-4b3f-0410-a08c-e77ef4b4d34b>
2008-09-03 15:04:03 +0000

    git-svn-id: https://mymoneylog.googlecode.com/svn/trunk@123 22c9decc-4b3f-0410-a08c-e77ef4b4d34b

--- javascripts/mymoneylog/base.js
+++ javascripts/mymoneylog/base.js
@@ -93,8 +93,8 @@ mlog.base = function() {
 			content = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" " +
 				"\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">" +
 				"<html xmlns=\"http://www.w3.org/1999/xhtml\" lang=\"en\" xml:lang=\"en\">" + 
-				"<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />" +
-				"<head><title>myMoneyLog Data File</title></head><body><pre id=\"data\">\n" +
+				"<head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />" +
+				"<title>myMoneyLog Data File</title></head><body><pre id=\"data\">\n" +
 				content +
 				"</pre></body></html>";
       if ($.browser.mozilla) {
